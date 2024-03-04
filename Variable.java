9a0db50179b7ad6efc8ed12a560ccc36c37ca1ad
Harsh Dahiya <123488839+believeharsh@users.noreply.github.com>
2024-03-04 12:07:45 +0530

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,9 +1,11 @@
 
-public class Variable {
+public class Variables {
     public static void main(String[] args) {
-        String name = "Harsh ";
+        String name = "Harsh";
         String name2 = "Dahiya";
-        System.out.println(name + name2);
-        // System.out.println(name2.replace("H", "P"));
+        System.out.println(name2.charAt(0));
+        String name3 = name.replace("H", "k");
+         System.out.println(name3);
     }
+    
 }
