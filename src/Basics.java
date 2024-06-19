a7d2e8fd77346caa9f995de1f188a09a386e7520
Believeharsh <bontinue@gmail.com>
2024-06-19 22:05:25 +0530

    added out folder for class files

--- src/Basics.java
+++ src/Basics.java
@@ -1,3 +1,4 @@
+package src;
 public class Basics {
    public static void main(String[] args){
       System.out.println(
--- src/First.java
+++ src/First.java
@@ -1,3 +1,4 @@
+package src;
 public class First{
     public static void main(String[] args) {
       System.out.println("hello-world");
