184650c62654e0ae26fbc27e15228a0f7cb4c594
Believeharsh <bontinue@gmail.com>
2024-07-01 21:17:55 +0530

    added Temperature.java

---
+++ src/Temperature.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        System.out.println("Calcious to Farenhite Converter **");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Enter temperature in Calcious : ");
+        float Tempc = in.nextFloat();
+        float TempF = (Tempc * 9/5) + 32 ; 
+        System.out.println("Temperature in Farenhite = " + TempF);
+
+    }
+}
