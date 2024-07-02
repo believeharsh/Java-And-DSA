b40943b5a44211bc65c6e4fc630b0a860238a215
Believeharsh <bontinue@gmail.com>
2024-07-02 23:14:01 +0530

    added Largest.java and Loops.java

---
+++ src/Largest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Largest {
+
+    public static void main(String[] args) {
+        System.out.println("Largest Number Chacker Program");
+
+        System.out.println("Enter three Number to check the Largest Numbers");
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int max = a;
+        if (b > max) {
+            max = b;
+
+        }
+        if (c > max) {
+            max = c;
+        }
+
+        System.out.println("Maximum Number is " + max);
+    }
+}
---
+++ src/Loops.java
@@ -0,0 +1,19 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("Loops In Java");
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println("Hello World!!");
+
+        // }
+        int i = 5;
+        // while (i <= 10 ) {
+        //     System.out.println("While condition gets true for the " + i + " time ");
+        //     i += 1 ; 
+        // }
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
