4a4363475dd3237f816cfb9d3227f2d0901384a5
Believeharsh <bontinue@gmail.com>
2024-07-03 18:57:16 +0530

    added SimpleCalc.java

---
+++ src/ReverseNum.java
@@ -0,0 +1,14 @@
+public class ReverseNum {
+    public static void main(String[] args) {
+        System.out.println("Rerverse the Number ::");
+        int n = 23456 ;  // test case 1
+        // int n = 454545; // test case 2
+        int ans = 0 ; 
+        while (n > 0 ) {
+            int rem = n % 10 ; // 6 5 4 3 2 
+            n /= 10 ;  // 2345 234 23 2 
+            ans = ans * 10 + rem ; // 6 65 654 6543 
+        }
+        System.out.println(ans);
+    }
+}
---
+++ src/SimpleCalc.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SimpleCalc {
+    // NOTE 
+    // Simple program like this makes you comfortable on using loops and how they actually behave in the code
+    public static void main(String[] args) {
+        System.out.println("Simple Calculator program in java");
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+
+        while (true) {
+            System.out.println("Enter the operator first");
+            char op = in.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("Enter Two Numbers for the operation");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+
+                }
+
+            }else if (op == 'x' || op == 'X') {
+                break ; 
+            } else {
+                System.out.println("invalid operation ");
+            }
+            System.out.println("The Answer is " + ans);
+
+        } 
+        in.close();
+    }
+
+}
