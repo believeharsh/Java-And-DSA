d04a9d709affe1d136d115d444959501a226ef47
Believeharsh <bontinue@gmail.com>
2024-07-08 22:00:54 +0530

    added Factorial.java

---
+++ src/Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    public static void main(String[] args) {
+       int ans =  getfactorial(3); 
+       System.out.println(ans);
+    }
+    static int getfactorial(int num) {
+        int factorial = 1 ; 
+        int i = 1 ;
+      
+        while ( i <= num){
+      
+            factorial *= i ; 
+            i++ ; 
+        }
+      return factorial ; 
+    }
+}
