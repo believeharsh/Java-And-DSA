afc6c965a1e28df7ee6d96288c176aff930ddf50
Believeharsh <bontinue@gmail.com>
2024-07-08 13:21:23 +0530

    understood Prime Numbers

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,26 +3,35 @@ import java.util.Scanner;
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
-        boolean ans = primecheckar(number);
-        System.out.println(ans);
-        in.close(); 
+        System.out.println("Give the Number to check it's prime or not ? ");
+        Scanner in =  new Scanner(System.in);
+        int number = in.nextInt(); 
+       boolean ans =  primecheck(number); 
+       System.out.println(ans);
+
+        in.close();
 
     }
-
-    static boolean primecheckar(int number) {
-        int c = 2;
+    static boolean primecheck(int number) {
         if (number <= 1) {
-            return false;
+            return false; 
         }
-        while (c * c <= number) {
-            if (number % c == 0) {
-                return false;
+        if (number == 2) {
+            return true; // 2 is a prime number
+        }
+        if (number % 2 == 0) {
+            return false; // Even numbers other than 2 are not prime
+        }
+        
+        int count = 3;
+        while (count * count <= number) {
+            if (number % count == 0) {
+                return false; 
             }
-            c++ ; 
+            count += 2; // Increment by 2 to check only odd numbers
         }
-        return true;
-
+        return true ; 
     }
 }
+
+
