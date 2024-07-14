bd636a00d538b033e71b2b729345cc8331123921
Believeharsh <bontinue@gmail.com>
2024-07-14 11:03:36 +0530

    added ArrIndxSwap.java

---
+++ src/SimpleProblems/ArrIndxSwap.java
@@ -0,0 +1,17 @@
+package SimpleProblems;
+
+import java.util.Arrays;
+
+public class ArrIndxSwap {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println("This is the original one" + Arrays.toString(arr));
+        swap(arr, 0, 1); 
+        System.out.println("After swaping " + Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2]; 
+        arr[index2] = temp ; 
+    }
+}
