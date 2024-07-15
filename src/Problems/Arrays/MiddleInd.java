85d969fc9992a5ebeed5104966b3ac56f46178f9
Believeharsh <bontinue@gmail.com>
2024-07-15 16:41:09 +0530

    added another approach for this question

--- src/Problems/Arrays/MiddleInd.java
+++ src/Problems/Arrays/MiddleInd.java
@@ -11,13 +11,16 @@ public class MiddleInd {
         int[] case3 = {2,5};
 
        int ans1 =  FindMidInd(case1);
+    //    int ans1 =  findMiddleIndex(case1);
        System.out.println(ans1);
        int ans2 =  FindMidInd(case2);
        System.out.println(ans2);
        int ans3 =  FindMidInd(case3);
        System.out.println(ans3);
     }
+    // first approach 
     static int FindMidInd(int[] nums) {
+
         int sum  = 0 ; 
         int temp = 0 ; 
 
@@ -36,4 +39,22 @@ public class MiddleInd {
         System.out.println("No Index is found" );
         return -1 ; 
     }
+    static int findMiddleIndex(int[] nums) {
+        if(nums.length <= 2) {
+          return -1;
+        };
+        int sum = 0 ; 
+        for(int i = 0 ; i < nums.length ; i++){
+          sum += nums[i];
+  
+        }
+        int leftsum = 0 ; 
+        for(int j = 0 ; j < nums.length ; j++){
+          int rightsum = sum - leftsum - nums[j];
+          if(leftsum == rightsum) return j;
+          leftsum += nums[j];
+        }
+        return -1 ; 
+          
+      }
 }
