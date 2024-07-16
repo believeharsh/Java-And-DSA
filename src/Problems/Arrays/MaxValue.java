fd76fa568dc076ea5128f64fa3f615beb0590855
Believeharsh <bontinue@gmail.com>
2024-07-16 09:37:52 +0530

    added MaxValueRange function

--- src/Problems/Arrays/MaxValue.java
+++ src/Problems/Arrays/MaxValue.java
@@ -4,11 +4,29 @@ public class MaxValue {
 
     public static void main(String[] args) {
         int[] arr = {9 , 20 , 45, 29, 90, 570};
+        System.out.print("This is the max value of array = ");
         System.out.println(max(arr));
+
+        int[] arr2 = {23, 4, 5, 10, 110, 80}; 
+        System.out.print("Here is the Maxvalue between Range = ");
+        System.out.println(maxValRange(arr2, 2, 5));
+
+        System.out.println();
     }
     static int max(int[] arr) {
         int tempMax = arr[0]; 
         for(int i = 1 ; i < arr.length ; i++) {
+            if(tempMax < arr[i]){
+                tempMax = arr[i];
+            }
+
+        }
+        return tempMax ; 
+    }
+    static int maxValRange(int[] arr, int start, int end) {
+        int tempMax = arr[start]; 
+        int index = start + 1 ; 
+        for(int i = index ; i <= end ; i++) {
             if(tempMax < arr[i]){
                 tempMax = arr[i];
             }
