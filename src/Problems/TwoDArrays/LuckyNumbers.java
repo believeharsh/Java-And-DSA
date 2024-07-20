dde0c31e3ec2845225ba88f1fb6e6d1161c80962
Believeharsh <bontinue@gmail.com>
2024-07-20 23:48:47 +0530

    formated

--- src/Problems/TwoDArrays/LuckyNumbers.java
+++ src/Problems/TwoDArrays/LuckyNumbers.java
@@ -12,40 +12,40 @@ public class LuckyNumbers {
 
     public static void main(String[] args) {
         int[][] matrix = {
-           {7,3,8},
-           {9,11,13},
-           {17,12,15}
-         
+                { 7, 3, 8 },
+                { 9, 11, 13 },
+                { 17, 12, 15 }
+
         };
 
-      System.out.println(luckyNumbers(matrix)) ; 
+        System.out.println(luckyNumbers(matrix));
     }
-static List<Integer> luckyNumbers(int[][] matrix){
-    List<Integer> luckyNumbers = new ArrayList<>(); 
-    for (int i = 0; i < matrix.length; i++) {
-        int minRowVal = matrix[i][0] ; 
-        int minRowValind = 0 ; 
 
-        for (int j = 1; j < matrix[i].length; j++) {
-            if(matrix[i][j] < minRowVal){
-                minRowVal = matrix[i][j] ; 
-                minRowValind = j ; 
+    static List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> luckyNumbers = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            int minRowVal = matrix[i][0];
+            int minRowValind = 0;
+
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] < minRowVal) {
+                    minRowVal = matrix[i][j];
+                    minRowValind = j;
+                }
+            }
+
+            boolean islucky = true;
+            for (int k = 0; k < matrix.length; k++) {
+                if (matrix[k][minRowValind] > minRowVal) {
+                    islucky = false;
+                    break;
+                }
+            }
+            if (islucky) {
+                luckyNumbers.add(minRowVal);
             }
         }
-
-        boolean islucky = true ; 
-        for (int k = 0; k < matrix.length; k++) {
-            if(matrix[k][minRowValind] > minRowVal){
-                islucky = false ; 
-                break ; 
-            }
-        }
-        if(islucky){
-            luckyNumbers.add(minRowVal);
-        }
+        return luckyNumbers;
     }
-    return luckyNumbers ; 
-}
-
 
 }
