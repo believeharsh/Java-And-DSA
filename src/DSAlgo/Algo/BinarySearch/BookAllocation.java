60deb1199b431f538eea3456e0cb9bfab99a9490
Believeharsh <bontinue@gmail.com>
2024-07-30 19:01:52 +0530

    added This Hard Question of code360

---
+++ src/DSAlgo/Algo/BinarySearch/BookAllocation.java
@@ -0,0 +1,50 @@
+package DSAlgo.Algo.BinarySearch ;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BookAllocation {
+       public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n = 5;
+        int m = 4;
+        int ans = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    };
+
+    public static int countStudents(ArrayList<Integer> arr, int pages) {
+        int n = arr.size(); // size of array
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                // add pages to current student
+                pagesStudent += arr.get(i);
+            } else {
+                // add pages to next student
+                students++;
+                pagesStudent = arr.get(i);
+            }
+           
+        }
+        return students;
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // book allocation impossible
+        if (m > n)
+            return -1;
+
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+
+        for (int pages = low; pages <= high; pages++) {
+            if (countStudents(arr, pages) == m) {
+                return pages;
+            }
+        }
+        return low;
+    }
+
+}
