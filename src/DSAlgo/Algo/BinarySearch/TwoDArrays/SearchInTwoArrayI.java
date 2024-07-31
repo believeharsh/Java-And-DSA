f334bd136708af91f5a3badf632e34d5d9321bbc
Believeharsh <bontinue@gmail.com>
2024-07-31 22:46:39 +0530

    added  SearchInTwoArray.java

--- src/DSAlgo/Algo/BinarySearch/TwoDArrays/SearchInTwoArrayI.java
+++ src/DSAlgo/Algo/BinarySearch/TwoDArrays/SearchInTwoArrayI.java
@@ -2,16 +2,31 @@ package DSAlgo.Algo.BinarySearch.TwoDArrays;
 
 import java.util.Arrays;
 
-public class FindTargerInMatrix {
+public class SearchInTwoArrayI {
     public static void main(String[] args) {
+        // int[][] matrix = {
+        // {10, 20, 30, 40},
+        // {41, 42, 43, 44},
+        // {45, 50, 52, 61},
+        // };
+
+        // Note :
+        // if you are given a matrix which in entirly sorted (in which the last element
+        // of the previous row is lesser then the first element of the next row) then
+        // this solution will work but more optimised solution is there.
+
+        // And if you have the row and col vise sorted array then this is the optimised
+        // solution.
         int[][] matrix = {
-            {10, 20, 30, 40},
-            {41, 42, 43, 44}, 
-            {45, 50, 52, 61},
+                { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 28, 29, 37, 49 },
+                { 33, 34, 38, 50 }
         };
         System.out.println(Arrays.toString(search(matrix, 43))); // should print [1,2]
         System.out.println(Arrays.toString(search(matrix, 44))); // should print [1,3]
         System.out.println(Arrays.toString(search(matrix, 10))); // should print [0,0]
+        System.out.println(Arrays.toString(search(matrix, 49))); // should print [2,3]
 
     }
 
---
+++ src/DSAlgo/Algo/BinarySearch/TwoDArrays/SearchInTwoArrayII.java
@@ -0,0 +1,37 @@
+package DSAlgo.Algo.BinarySearch.TwoDArrays;
+
+
+public class SearchInTwoArrayII {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 10, 20, 30, 40 },
+                { 41, 42, 43, 44 },
+                { 45, 50, 52, 61 },
+                { 62, 65, 70, 71 }
+        };
+        int target = 70; //
+        System.out.println((searchMatrix(matrix, target)));
+
+    }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+
+    }
+
+}
