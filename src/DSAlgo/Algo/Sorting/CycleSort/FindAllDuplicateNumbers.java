ef2ed0d9293088383cdf782a8547f5e33b23a34a
Believeharsh <bontinue@gmail.com>
2024-08-07 17:44:04 +0530

    added FindAllDuplicates.java:

---
+++ src/DSAlgo/Algo/Sorting/CycleSort/FindAllDuplicateNumbers.java
@@ -0,0 +1,44 @@
+package DSAlgo.Algo.Sorting.CycleSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicateNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 4, 5, 6, 7}; // Duplicate numbers are {2, 3}
+        int[] arr2 = {1, 1, 2, 2, 3, 3, 4, 4}; // Duplicate numbers are {1, 2, 3, 4}
+        System.out.println(findAllDuplicates(arr));
+        System.out.println(findAllDuplicates(arr2));
+    }
+
+    public static List<Integer> findAllDuplicates(int[] arr) {
+        int n = arr.length;
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            while (arr[i] != i + 1) {
+                int rightInd = arr[i] - 1;
+                if (arr[i] != arr[rightInd]) {
+                    swap(arr, i, rightInd);
+                } else {
+                    break; // break the loop if we find a duplicate
+                }
+            }
+        }
+
+        // Now check for duplicates
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                ans.add(arr[i]);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
