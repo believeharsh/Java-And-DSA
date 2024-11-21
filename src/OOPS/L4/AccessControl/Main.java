20f3383bc3dc2689f26985d60ce742f64c930add
Believeharsh <bontinue@gmail.com>
2024-11-21 10:47:34 +0530

    OOPS Learning L4 Kunal Kushwaha : 21 Nov 2024

---
+++ src/OOPS/L4/AccessControl/Main.java
@@ -0,0 +1,10 @@
+package OOPS.L4.AccessControl;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjMethods obj = new ObjMethods() ; 
+        System.out.println(obj.getClass());
+        System.out.println( obj instanceof ObjMethods);
+  
+    }
+}
---
+++ src/OOPS/L4/AccessControl/ObjMethods.java
@@ -0,0 +1,12 @@
+package OOPS.L4.AccessControl;
+
+public class ObjMethods {
+
+    class Harsh {
+        String name = "Harsh Dahiya" ; 
+        
+    }
+    class ChildHarsh extends Harsh {
+      String name = "Nishi Dahiya" ; 
+    }
+}
