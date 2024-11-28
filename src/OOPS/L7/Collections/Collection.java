b001e1995daa03939bc591dd18f07a1045b57750
Believeharsh <bontinue@gmail.com>
2024-11-28 23:22:14 +0530

    28 Nov 2024 : OOP Learnings

---
+++ src/OOPS/L7/Collections/Collection.java
@@ -0,0 +1,10 @@
+package  OOPS.L7.Collections ; 
+
+public class Collection {
+
+    // what is the Collection FrameWork in java?
+    // the Vector class is a part of the Collection Framework, located in the java.util package. It provides a dynamic array that can grow or shrink as needed, similar to the ArrayList class. However, Vector is synchronized, making it thread-safe but generally slower than ArrayList.
+   public static void main(String[] args) {
+    
+   }
+}
