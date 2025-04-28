5a409d0f161e7a19e52dd572546bfd53015743bd
Believeharsh <bontinue@gmail.com>
2025-04-28 12:11:35 +0530

    Java-DSA - Day 9 : Linear Search

---
+++ src/DSAlgo/Algo/LinearSearch/Theory.java
@@ -0,0 +1,22 @@
+// what is the Linear serach algorithim 
+
+// It is simple searching algorithm used to find a particular value in the list. It works by swquentially checking each element of the list until the desired value is found or list is exhausted. 
+
+
+// steps : start from the first element ; 
+        //  : compare each element ; 
+        //  : check for the match 
+        //    -------> : if match found, return that index ; 
+                    // : if now match, proceed to next element ; 
+        //  : End of the List : if reached the end without finding the target, return -1 
+
+
+// Chararcterstics : 
+    // TC : O(n) - in worst case, Algorithm checks each element ; 
+    // SC : O(1) - constant amount of extra space used. 
+    // Data Structure : - Applicable to any array or list, whether sorted or unsorted. 
+
+// Advantages : 1. Simple and easy to implement. 
+            //  2. Works on both sorted or unsorted 
+            
+// Disadvantages : 1. Inefficient for large lists due to its ,inear time    complexity. 
