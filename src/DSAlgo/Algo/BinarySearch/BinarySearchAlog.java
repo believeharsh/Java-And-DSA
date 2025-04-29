7937d329ddeeee117c2f440ec56fca5ed17ff276
Believeharsh <bontinue@gmail.com>
2025-04-29 14:13:35 +0530

    Java-DSA : Day 10 - Binary Search Implementtion

--- src/DSAlgo/Algo/BinarySearch/BinarySearchAlog.java
+++ src/DSAlgo/Algo/BinarySearch/BinarySearchAlog.java
@@ -6,7 +6,7 @@ public class BinarySearchAlog {
         int[] arr2 = { 60, 50, 40, 30, 20, 10};
         int target = 40;
         System.out.println("Index of target: " + IndexOfTarget(arr1, target));
-        // System.out.println("Index of target: " + IndexOfTarget2(arr2, target));
+        System.out.println("Index of target: " + IndexOfTarget2(arr2, target));
 
 
     }
@@ -30,6 +30,46 @@ public class BinarySearchAlog {
 
         return -1; // target not found
     }
+    
+        // what if the we don't know whether array is sorted in ascending order or
+    // descending order
 
+    // - 📌 Order-Agnostic Binary Search
+    // - Works on both ascending and descending sorted arrays.
+    // - First determine order using first and last elements.
+    // - Then apply binary search logic accordingly.
+    // - Time Complexity: O(log n)
+
+    static int IndexOfTarget2(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        boolean ascending = arr[low] < arr[high];
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (ascending) {
+                if (target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+        }
+
+        return -1; // target not found
+    }
 
 }
