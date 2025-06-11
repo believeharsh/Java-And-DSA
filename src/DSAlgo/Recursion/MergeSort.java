9b3a8c0256fb015b0e658a7427fe0738f6fda68c
Believeharsh <bontinue@gmail.com>
2025-06-11 08:57:00 +0530

    Java-DSA : Day 29 -> MergeSort In place

--- src/DSAlgo/Recursion/MergeSort.java
+++ src/DSAlgo/Recursion/MergeSort.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 3, 2, 1 };
-        int[] ans = mergeSort(arr);
-        System.out.println(Arrays.toString(ans));
+        // int[] arr = { 4, 5, 1, 3, 2 };
+        // int[] ans = mergeSort(arr);
+        // System.out.println(Arrays.toString(ans));
+
+        int[] arr = {5, 4, 3, 1, 2} ; 
+        mergeSortInPlace(arr, 0, arr.length) ;     
+        System.out.println(Arrays.toString(arr));
     }
 
     static int[] mergeSort(int[] arr) {
@@ -14,7 +18,6 @@ public class MergeSort {
             return arr;
         }
 
-        
         int mid = arr.length / 2;
 
         int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
@@ -57,4 +60,55 @@ public class MergeSort {
 
         return mix;
     }
+
+    static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s + l] = mix[l];
+        }
+    }
+
 }
