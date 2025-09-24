273d3a4e5e455822139f4a2729771fede318c6e5
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-09-24 09:23:07 +0530

    DSA : 100/3 Bubble and Dutch sort algo

--- src/DSAlgo/Algo/Sorting/BubbleSort/SortTheArr.java
+++ src/DSAlgo/Algo/Sorting/BubbleSort/SortTheArr.java
@@ -1,50 +1,59 @@
 package DSAlgo.Algo.Sorting.BubbleSort;
 
-// problem link : https://leetcode.com/problems/sort-colors/ 
-// Here we are using the Bubble sort technique. 
 import java.util.Arrays;
 
 public class SortTheArr {
 
     public static void main(String[] args) {
-        int[] arr = {5, 4, 2, 3, 1} ; 
-        BubbleSort(arr) ; 
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {1, 2, 3, 4, 5 };
+        // Use a single BubbleSort method for clarity
+        bubbleSortAscending(arr);
+        System.out.println("Ascending Order: " + Arrays.toString(arr));
 
+        int[] arr2 = {5, 4, 2, 3, 1};
+        bubbleSortDescending(arr2);
+        System.out.println("Descending Order: " + Arrays.toString(arr2));
     }
-
-    public static void BubbleSort(int[] arr) {
+    
+    // For Ascending Order
+    public static void bubbleSortAscending(int[] arr) {
         int n = arr.length;
         boolean swapped;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             swapped = false;
-
-            // this is the loop if we want descending order ; 
-            for (int j = 1; j < n - i; j++) {
-                if (arr[j] > arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
                     swapped = true;
-
                 }
             }
-            // this is the loop if we want ascending order : 
-            // for (int j = 1; j < n - i; j++) {
-            //     if (arr[j] < arr[j - 1]) {
-            //         int temp = arr[j];
-            //         arr[j] = arr[j-1];
-            //         arr[j-1] = temp;
-            //         swapped = true;
-
-            //     }
-            // }
-            if(!swapped) {
-                break ; 
+            if (!swapped) {
+                break;
             }
         }
-       
+    }
+    
+    // For Descending Order
+    public static void bubbleSortDescending(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
